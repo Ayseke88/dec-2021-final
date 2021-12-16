ff803a7bd16a2297165765c5cfd1ea3385f9eb2f
Ayseke88 <91011188+Ayseke88@users.noreply.github.com>
2021-12-16 23:56:32 +0600

    доработала страничку логин

---
+++ FinalWork/script/login.js
@@ -0,0 +1,16 @@
+// Toggle Form
+const container = document.querySelector(".container");
+const inputs = document.querySelectorAll(".form-box input[type = 'password']");
+const icons = [...document.querySelectorAll(".form-icon")];
+const spans = [...document.querySelectorAll(".form-box .top span")];
+const section = document.querySelector("section");
+
+spans.map((span) => {
+  span.addEventListener("click", (e) => {
+    const color = e.target.dataset.id;
+    container.classList.toggle("active");
+    section.classList.toggle("active");
+    document.querySelector(":root").style.setProperty("--custom", color);
+  });
+});
+
