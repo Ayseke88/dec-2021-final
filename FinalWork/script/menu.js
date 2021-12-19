22044623da0c8b9906ff97d5f2b944c69b3a4e31
NecomaV <73179504+NecomaV@users.noreply.github.com>
2021-12-20 02:12:16 +0600

    Full responsive header

--- FinalWork/script/menu.js
+++ FinalWork/script/menu.js
@@ -1,6 +1,11 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navLinks = document.getElementsByClassName('links-container')[0]
+const navL = document.querySelectorAll('links-container li') ;
 
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active')
   })
+
+  // navL.forEach((link,index) => {
+  //   link.style.animation = `navLFade 0.5s ease forwards ${index / 7 + 2}s`;
+  // })
