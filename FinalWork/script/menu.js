78462923a064c05789b4b97464be7caf3042b924
NecomaV <73179504+NecomaV@users.noreply.github.com>
2021-12-24 16:44:08 +0600

    Update menu.js

--- FinalWork/script/menu.js
+++ FinalWork/script/menu.js
@@ -5,7 +5,3 @@ const navL = document.querySelectorAll('links-container li') ;
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active')
   })
-
-  // navL.forEach((link,index) => {
-  //   link.style.animation = `navLFade 0.5s ease forwards ${index / 7 + 2}s`;
-  // })
